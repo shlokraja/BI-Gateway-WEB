9283c2a15448d7d68053294b7618e3fa8cd489c5
shlokraja <raja.s@shloklabs.com>
2016-11-24 10:48:26 +0530

    logo and color theme changes
    logo and color theme changes

--- public/js/vendor/highcharts.js
+++ public/js/vendor/highcharts.js
@@ -1491,8 +1491,8 @@ defaultLabelOptions = {
 };
 
 defaultOptions = {
-	colors: ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', '#492970',
-		'#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
+	colors: ['#fbb713', '#3c2a01', '#dd9c03', '#3c2a01', '#1aadce', '#FCC74A',
+		'#FDD372', '#FDCD5E', '#FEE6AF', '#FEECC3'],
 	symbols: ['circle', 'diamond', 'square', 'triangle', 'triangle-down'],
 	lang: {
 		loading: 'Loading...',
